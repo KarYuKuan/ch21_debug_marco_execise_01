0dd38b09cc1065a43b26a4b8f5d4d7cdf78e0e77
Samurai_Chia <jyjzone@163.com>
2024-12-08 17:30:02 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use log::debug;
+
 fn main() {
-    println!("Hello, world!");
+    let rec1 = Rectangle{
+        width:32,
+        height:44,
+    };
+    dbg!(&rec1);
+}
+
+#[derive(Debug)]
+struct Rectangle{
+    width:i32,
+    height:i32,
+}
+
+fn area(rectangle:&Rectangle)->i32{
+    rectangle.height*rectangle.width
 }
